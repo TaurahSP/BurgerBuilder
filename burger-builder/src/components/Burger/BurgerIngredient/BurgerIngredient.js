3b39f5ae0720e4868fafd9ade65c824f1ece8501
Keshav Taurah <keshavtaurah@yahoo.com>
2018-10-01 15:08:23 +0400

    burger dynamic ingredient

--- burger-builder/src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ burger-builder/src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -1,11 +1,15 @@
-import React from 'react';
+import React ,{Component} from 'react';
 import classes from './BurgerIngredient.css';
+import PropTypes from 'prop-types';
 
-const burgerIngredient=(props)=>{
+
+class BurgerIngredient extends Component{
+
+    render(){
 
     let ingredient=null;
 
-    switch(props.type){
+    switch(this.props.type){
 
         case('bread-bottom'):
         ingredient=<div className={classes.BreadBottom}></div>;
@@ -34,6 +38,12 @@ const burgerIngredient=(props)=>{
     }
 
     return ingredient;
+}
 };
 
-export default burgerIngredient;
+BurgerIngredient.PropTypes={
+
+    type:PropTypes.string.isRequired
+}
+
+export default BurgerIngredient;
